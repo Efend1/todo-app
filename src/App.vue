6944e327cee8d7de169e76cd18d1e759c3a5b09d
Efend1 <kujanglarang31@gmail.com>
2024-10-08 15:07:04 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted, computed, watch } from "vue";
+
+const todo = ref([]);
+const name = ref("");
+const input_content = ref("");
+const category = ref(null);
+
+const todo_asc = computed(() =>
+  todo.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  }),
+);
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="app">
+    <section class="greetings">
+      <h2 class="tittle">
+        Halo Apakabar?
+        <input type="text" placeholder="Nama Di sini" v-model="name" />
+      </h2>
+    </section>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
